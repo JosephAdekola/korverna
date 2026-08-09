f129a0c7b37ec51d0872758bd8ae0107701a65fd
josephAdekola <adekola.adebayo2@gmail.com>
2026-08-09 17:40:36 +0100

    fix: rewring url

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const hostname = request.headers.get("host")?.toLocaleLowerCase()
+//   const hostname = request.headers.get("host")?.toLocaleLowerCase()
 
-  console.log({hostname});
-  
+const reqUrl = request.url
 
-  if (hostname?.startsWith("infrastructure")) {
+  if (reqUrl.startsWith("https://www.infrastructure")) {
     return NextResponse.rewrite(
       new URL("/infrastructure", request.url)
     );
-  } else if(hostname?.startsWith("korverna")) {
-    return NextResponse.rewrite(
-        new URL("/welcome", request.url)
-    )
   }
 
-//   return NextResponse.rewrite(
-//     new URL("/welcome", request.url)
-//   );
+  return NextResponse.rewrite(
+    new URL("/welcome", request.url)
+  );
 }
 
 export const config = {
